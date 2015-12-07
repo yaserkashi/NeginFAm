91f73400e55b4880c773237d984b10dba36b27d2
Fateme Kamali <kamali.fateme@gmail.com>
2015-12-07 10:49:11 +0330

    لاگین و لاگ اوت به صفحه اول اضافه شد همه کلاس های جاوا به جز تست برای انجام لاگین و لاگ اوت است کلاس تست برای تست کردن اتصال به دیتابیس نوشته شده است

---
+++ src/java/com/Baloot/login/DataConnect.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.Baloot.login;
+
+/**
+ *
+ * @author FK
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+ 
+public class DataConnect {
+ 
+    public static Connection getConnection() {
+        try {
+            Class.forName("org.postgresql.Driver");
+            Connection con = DriverManager.getConnection(
+                    "jdbc:postgresql://127.0.0.1:5432/neginfam", "postgres", "1234");
+            return con;
+        } catch (Exception ex) {
+            System.out.println("Database.getConnection() Error -->"
+                    + ex.getMessage());
+            return null;
+        }
+    }
+ 
+    public static void close(Connection con) {
+        try {
+            con.close();
+        } catch (Exception ex) {
+        }
+    }
+}
+
