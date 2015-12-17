92056a9e0726736ee08135cbf4574cac700a7b07
unknown <yaser.kashialashti@yahoo.com0>
2015-12-17 16:03:52 +0330

    اضافه کردن کارای خانم جانفدا به پروژه

---
+++ src/java/com/Baloot/page/ImagesView.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.Baloot.page;
+
+/**
+ *
+ * @author Ali-M
+ */
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+ 
+@ManagedBean
+public class ImagesView {
+     
+    private List<String> images;
+     
+    @PostConstruct
+    public void init() {
+        images = new ArrayList<String>();
+        for (int i = 1; i <= 10; i++) {
+            images.add(i + ".jpg");
+        }
+    }
+ 
+    public List<String> getImages() {
+        return images;
+    }
+}
