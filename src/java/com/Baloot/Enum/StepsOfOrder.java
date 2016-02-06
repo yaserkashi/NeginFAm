f616c48f164108035ad5549dd4cda4dbaf4306b2
Ali Asti <Ali41asti@gmail.com>
2016-02-06 13:52:55 -0800

    enum مربوط به وضعیت های سفارش اضافه شد

---
+++ src/java/com/Baloot/Enum/StepsOfOrder.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.Baloot.Enum;
+
+/**
+ *
+ * @author Ali
+ */
+public enum StepsOfOrder {
+    //registrationOrder=ثبت سفارش,registrationFactor=صدور فاکتور,payFactor=پرداخت فاکتور توسط مشتری
+    //,ConfirmationPayFactor=تایید پرداخت توسط مدیر,EndOrder=پایان کار
+    registrationOrder,registrationFactor,payFactor,ConfirmationPayFactor,EndOrder
+    
+}
