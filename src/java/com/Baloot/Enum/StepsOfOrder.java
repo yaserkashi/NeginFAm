cd5801168fe5880675b45fd7700cc3fb723b4f31
Ali Asti <Ali41asti@gmail.com>
2016-02-07 15:43:49 +0330

    توضیحات نوع شمارشی مراحل سفارش به شکل صحیح اضافه شد

--- src/java/com/Baloot/Enum/StepsOfOrder.java
+++ src/java/com/Baloot/Enum/StepsOfOrder.java
@@ -7,12 +7,38 @@
 package com.Baloot.Enum;
 
 /**
- *
- * @author Ali
- */
+* <h3>توضیحات</h3>
+*  این مقدارشمارشی برای تعیین مراحل 
+* یک سفارش است
+* @author  Ali Asti
+* @version 1.0
+* @since   2014-03-31 
+*/
 public enum StepsOfOrder {
-    //registrationOrder=ثبت سفارش,registrationFactor=صدور فاکتور,payFactor=پرداخت فاکتور توسط مشتری
-    //,ConfirmationPayFactor=تایید پرداخت توسط مدیر,EndOrder=پایان کار
-    registrationOrder,registrationFactor,payFactor,ConfirmationPayFactor,EndOrder
+    /**
+     *  registrationOrder=ثبت سفارش
+     */
+    registrationOrder,
+    /**
+     *  ,registrationFactor=صدور فاکتور
+     */
+    registrationFactor,
+    /**
+     * payFactor=فاکتور توسط مشتری پرداخت شده است
+     */
+    payFactor,
+    /**
+     * فاکتور توسط مدیر تایید پرداخت شده است
+     * و کار آغاز شده است
+     */
+    ConfirmationPayFactor,
+    /**
+     * کار تمام شده و فایل پروژه برای مشتری آپلود شده است
+     */
+    EndOrder,
+    /**
+     * سفارش کنسل شده است
+     */
+    dissuasion
     
 }
