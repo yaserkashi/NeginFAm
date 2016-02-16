9a861f05ccafd0d4fcc2f913456a5d1d438c94e2
Ali Asti <Ali41asti@gmail.com>
2016-02-16 23:14:12 +0330

    قسمت کد مربوط به فراموشی رمز درست شد پیغام مناسب +ری دایرکت

--- src/java/login/ForgotPassword.java
+++ src/java/login/ForgotPassword.java
@@ -10,7 +10,9 @@ import com.Baloot.util.*;
 import java.math.BigInteger;
 import java.util.Random;
 import java.security.SecureRandom;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
 /**
  *کلاس فراموشی رمز 
  * این کلاس به صفحه ای که بعد از زدن دکمه 
@@ -49,26 +51,35 @@ public class ForgotPassword {
      * تابع عوض کردن رمز کاربر و ارسال ایمیل مناسب به کاربر
      * 
      */
-    public void changePassword()
+    public String changePassword()
     { 
         try {
             if(userName!=null&&email!=null)
             {
-                System.out.println("gsfdjkdsjkg");
+                
                 SecureRandom random=new SecureRandom();
                 BigInteger bigInteger=new BigInteger(30, random);
                 String newPssWord=bigInteger.toString(32);
                 Users user=UserServices.getUserByUsername(userName);
                 if(user!=null && user.getEmail().equals(email))
                 {
-                    System.out.println("gsfdjkdsj+++++++++++++++++++++++++++++++++++++++kg");
                 Sendemail.Send("neginfamardakan", "09134512648",email,"تغییر زمر عبور",newPssWord);
                 UserServices.changePassword(user.getId(), newPssWord);
+                FacesContext.getCurrentInstance().addMessage(null,
+                new FacesMessage("زمر عبور جدید برای شما ارسال شد!"));
+                return "/index.xhtml";
                 }
+                FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage("نام کاربری و ایمیل تطابق ندارد!"));
+                return null;
             }
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage("نام کاربری یا ایمیل خالی است!"));
+                return null;
         } catch (Exception e) {
             e.getMessage();
         }
+        return null;
     }
     
 }
